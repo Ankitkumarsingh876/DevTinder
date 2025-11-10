9033ab45329a4f9470673d838db5be9ae923518b
Ankit Kumar Singh <singhankittomar.23@gmail.com>
2025-11-10 14:45:14 +0530

    Explore , Get , Post, delete and use order of code matter a lot

--- src/app.js
+++ src/app.js
@@ -2,15 +2,26 @@ const express = require("express");
 
 const app = express();
 
-app.get("/" , (req,res) => {
-     res.send("hello hello !");
+
+app.get("/user" , (req,res) => {
+     res.send({firstName: "Ankit Kumar" , LastName: "singh"});
 });
-app.get("/test" , (req,res) => {
-     res.send("namaste node");
-});
-app.get("/hello" , (req,res) => {
-     res.send("namaste ");
-});
+
+app.post ("/user", (req, res) => {
+     res.send("Data succefully saved to the database");
+})
+app.delete("/user", (req,res) => {
+     res.send("Delete the data");
+})
+// app.use("/test" , (req,res) => {
+//      res.send("namaste node");
+// });
+// app.use("/hello" , (req,res) => {
+//      res.send("namaste ");
+// });
+// app.use("/" , (req,res) => {
+//      res.send("hello hello !");
+// });
 
 
 app.listen(7777, () => {
