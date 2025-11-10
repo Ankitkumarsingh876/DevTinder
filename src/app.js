98c6066fc6d77c7d1fc1d8ae8b19aea54afbc31b
Ankit Kumar Singh <singhankittomar.23@gmail.com>
2025-11-10 19:15:50 +0530

    Playing with multiple router handler

--- src/app.js
+++ src/app.js
@@ -2,6 +2,38 @@ const express = require("express");
 
 const app = express();
 
+// app.use("/route", rh1, [rh2, rh3], rh4, rh5);
+// if i put array all the route handler or some of the router eg: 2,3 ,
+// then this will not effect on my resoponse or a code
+
+app.use("/user", (req,res,next) => {
+     console.log("handling route user1!!");
+     // res.send("Response1!!");
+     next()
+}, 
+    (req,res,next) => {
+     console.log("handling route user2!!");
+     // res.send("Response2!!");
+     next()
+},
+  (req,res,next) => {
+     console.log("handling route user3!!");
+     // res.send("Response3!!");
+     next()
+},
+  (req,res,next) => {
+     console.log("handling route user4!!");
+     // res.send("Response4!!");
+     next()
+},
+  (req,res,next) => {
+     console.log("handling route user5!!");
+     // res.send("Response5!!");
+     // next()
+     // res.send("Response5!!");
+}
+);
+
 
 // app.get("/user/:userID/:name/:password" , (req,res) => {
 //      console.log(req.params);
@@ -23,39 +55,39 @@ const app = express();
 //      res.send({firstName: "Ankit Kumar" , LastName: "singh"});
 // });
 
-app.get("/ab?c" , (req,res) => {
-     // console.log(req.query);
-     res.send({ LastName: "singh"});
-});
+// app.get("/ab?c" , (req,res) => {
+//      // console.log(req.query);
+//      res.send({ LastName: "singh"});
+// });
 
-app.get("/ab+c" , (req,res) => {
-     // console.log(req.query);
-     res.send({ LastName: "singh"});
-});
-app.get("/ab*cd" , (req,res) => {
-     // console.log(req.query);
-     res.send({ LastName: "singh"});
-});
-app.get("/a(bc)?d" , (req,res) => {
-     // console.log(req.query);
-     res.send({ LastName: "singh"});
-});
-app.get("/a(bc)+d" , (req,res) => {
-     // console.log(req.query);
-     res.send({ LastName: "singh"});
-});
-app.get("/a/" , (req,res) => {
-     // console.log(req.query);
-     res.send({ LastName: "singh"});
-});
-app.get("/./" , (req,res) => {
-     // console.log(req.query);
-     res.send({ LastName: "singh"});
-});
-app.get(/.*fly$/ , (req,res) => {
-     // console.log(req.query);
-     res.send({ LastName: "singh"});
-});
+// app.get("/ab+c" , (req,res) => {
+//      // console.log(req.query);
+//      res.send({ LastName: "singh"});
+// });
+// app.get("/ab*cd" , (req,res) => {
+//      // console.log(req.query);
+//      res.send({ LastName: "singh"});
+// });
+// app.get("/a(bc)?d" , (req,res) => {
+//      // console.log(req.query);
+//      res.send({ LastName: "singh"});
+// });
+// app.get("/a(bc)+d" , (req,res) => {
+//      // console.log(req.query);
+//      res.send({ LastName: "singh"});
+// });
+// app.get("/a/" , (req,res) => {
+//      // console.log(req.query);
+//      res.send({ LastName: "singh"});
+// });
+// app.get("/./" , (req,res) => {
+//      // console.log(req.query);
+//      res.send({ LastName: "singh"});
+// });
+// app.get(/.*fly$/ , (req,res) => {
+//      // console.log(req.query);
+//      res.send({ LastName: "singh"});
+// });
 // app.post ("/user", (req, res) => {
 //      res.send("Data succefully saved to the database");
 // })
