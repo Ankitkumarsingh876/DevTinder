e56fd6bae2edccbb5b0fcfba9fa6ae11b04a3df4
Ankit Kumar Singh <singhankittomar.23@gmail.com>
2025-11-21 17:24:07 +0530

    Write Feed API and Create SKIP and LIMIT on our page

--- src/Routers/user.js
+++ src/Routers/user.js
@@ -2,6 +2,7 @@ const express  = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const connectionRequestModel = require("../models/connectionRequest");
+const User = require("../models/user");
 
 
 userRouter.get("/user/request/recieved", userAuth, async(req,res) => {
@@ -46,6 +47,43 @@ userRouter.get("/user/connection", userAuth, async (req,res) => {
     catch(err){
         res.status(400).send("ERROR :" + err.message);
     }
+})
+
+userRouter.get("/feed", userAuth, async(req,res) => {
+    try{
+        const loggedInUser = req.user;
+
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page - 1) * limit;
+
+        const connectionRequest = await connectionRequestModel.find({
+            $or: [
+                {formUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ],
+        }).select("formUserId toUserId");
+        const hideUserFromFeed = new Set();
+        connectionRequest.forEach((req) => {
+            hideUserFromFeed.add(req.formUserId.toString());
+            hideUserFromFeed.add(req.toUserId.toString());
+        });
+
+        const users = await User.find({
+            $and: [{_id: {$nin: Array.from(hideUserFromFeed)},},
+                {_id: {$ne: loggedInUser._id},}
+            ],
+
+        }).skip(skip).limit(limit);
+
+        res.send(users);
+
+    }
+    catch(err){
+        res.status(400).send("ERROR :" + err.message)
+    }
+
 })
 
 module.exports = userRouter;
